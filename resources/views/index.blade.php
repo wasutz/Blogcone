fba568dadc72132c2a0acaba3afe900a35c5107f
Wasut <wasut_pao@hotmail.com>
2017-01-07 14:01:36 +0700

    add mock up index page for show post and sidebar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,69 @@
 
 @section('content')
 <div class="container">
-    Hello
+   <div class="row">
+		<div class="col-md-8">
+			<div class="card clearfix">
+				<h2 class="title"><a href="">Title</a></h2>
+				<div class="meta">
+					<span>by <a href="#">Admin</a></span> -
+					<span>On 5 January 2017 - 13:09</span>
+				</div>
+				<p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam excepturi, quia sequi, dicta magnam obcaecati quibusdam nobis nihil inventore reiciendis aut quo molestias voluptates id officiis consequatur iste eos repellat...</p>
+
+				<div class="pull-left">
+					<span>Likes 147</span>
+					<span>Comments 3</span>
+				</div>
+				<button class="btn btn-primary pull-right">Readmore</button>
+			</div>
+
+						<div class="card clearfix">
+				<h2 class="title"><a href="">Title</a></h2>
+				<div class="meta">
+					<span>by <a href="#">Admin</a></span> -
+					<span>On 5 January 2017 - 13:09</span>
+				</div>
+				<p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam excepturi, quia sequi, dicta magnam obcaecati quibusdam nobis nihil inventore reiciendis aut quo molestias voluptates id officiis consequatur iste eos repellat...</p>
+
+				<div class="pull-left">
+					<span>Likes 147</span>
+					<span>Comments 3</span>
+				</div>
+				<button class="btn btn-primary pull-right">Readmore</button>
+			</div>
+
+						<div class="card clearfix">
+				<h2 class="title"><a href="">Title</a></h2>
+				<div class="meta">
+					<span>by <a href="#">Admin</a></span> -
+					<span>On 5 January 2017 - 13:09</span>
+				</div>
+				<p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam excepturi, quia sequi, dicta magnam obcaecati quibusdam nobis nihil inventore reiciendis aut quo molestias voluptates id officiis consequatur iste eos repellat...</p>
+
+				<div class="pull-left">
+					<span>Likes 147</span>
+					<span>Comments 3</span>
+				</div>
+				<button class="btn btn-primary pull-right">Readmore</button>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="job card">
+				<h2 class="title">Job</h2>
+				<ul>
+					<li><a href="#">Job 1</a></li>
+					<li><a href="#">Job 2</a></li>
+				</ul>
+			</div>
+			<div class="tag card">
+				<h2 class="title">Tag</h2>
+				<ul>
+					<li><a href="#">Tag 1</a></li>
+					<li><a href="#">Tag 2</a></li>
+				</ul>
+			</div>
+        </div>
+   </div>
 </div>
 @endsection
