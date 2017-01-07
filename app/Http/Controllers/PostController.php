822bcd072ec12431941837f3da3ab4afecd1f5ad
Wasut <wasut_pao@hotmail.com>
2017-01-07 17:13:41 +0700

    add middleware auth, add store to create post, show to display post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePost;
+use App\Post;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');   
+    }
+
     public function index()
     {
-        //
+        return view('posts.index');
     }
 
     public function create()
@@ -16,19 +23,27 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store(Request $request)
+    public function store(StorePost $request)
     {
-        //
+        $post = Post::create([
+                    'title' => $request->title,
+                    'content' => $request->content,
+                    'user_id' => $request->user()->id
+                ]);
+
+        return redirect()->route('posts.show', $post->id);
     }
 
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('posts.show')->with(['post' => $post]);
     }
 
     public function edit($id)
     {
-        //
+        return view('posts.edit');
     }
 
     public function update(Request $request, $id)
