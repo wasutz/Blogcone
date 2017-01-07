fa05708a61743dd0ad218411142a7e1cd0648b86
Wasut <wasut_pao@hotmail.com>
2017-01-07 17:14:12 +0700

    add view show to display one post

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="card clearfix">
+		<h2 class="title"><a href="">{{ $post->title }}</a></h2>
+		<div class="meta">
+			<span>by <a href="#">{{ $post->user->username }}</a></span> -
+			<span>{{ $post->created_at->format('d M Y') }}</span>
+		</div>
+		<p class="content">{{ $post->content }}</p>
+
+		<div class="pull-left">
+			<span>Likes 147</span>
+			<span>Comments 3</span>
+		</div>
+	</div>
+@endsection
