52ada4da592487e9c70e0ae239a30bab42255459
Wasut <wasut_pao@hotmail.com>
2017-01-07 16:00:52 +0700

    delete container, move it to app.layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,42 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-   <div class="row">
-		<div class="col-md-8">
-			@foreach ($posts as $post)
-				<div class="card clearfix">
-					<h2 class="title"><a href="">{{ $post->title }}</a></h2>
-					<div class="meta">
-						<span>by <a href="#">Admin</a></span> -
-						<span>{{ $post->created_at->format('d M Y') }}</span>
-					</div>
-					<p class="content">{{ $post->content }}</p>
-
-					<div class="pull-left">
-						<span>Likes 147</span>
-						<span>Comments 3</span>
-					</div>
-					<button class="btn btn-primary pull-right">Readmore</button>
+<div class="row">
+	<div class="col-md-8">
+		@foreach ($posts as $post)
+			<div class="card clearfix">
+				<h2 class="title"><a href="">{{ $post->title }}</a></h2>
+				<div class="meta">
+					<span>by <a href="#">Admin</a></span> -
+					<span>{{ $post->created_at->format('d M Y') }}</span>
 				</div>
-			@endforeach
+				<p class="content">{{ $post->content }}</p>
+
+				<div class="pull-left">
+					<span>Likes 147</span>
+					<span>Comments 3</span>
+				</div>
+				<button class="btn btn-primary pull-right">Readmore</button>
+			</div>
+		@endforeach
+	</div>
+	<div class="col-md-4">
+		<div class="job card">
+			<h2 class="title">Job</h2>
+			<ul>
+				<li><a href="#">Job 1</a></li>
+				<li><a href="#">Job 2</a></li>
+			</ul>
 		</div>
-		<div class="col-md-4">
-			<div class="job card">
-				<h2 class="title">Job</h2>
-				<ul>
-					<li><a href="#">Job 1</a></li>
-					<li><a href="#">Job 2</a></li>
-				</ul>
-			</div>
-			<div class="tag card">
-				<h2 class="title">Tag</h2>
-				<ul>
-					<li><a href="#">Tag 1</a></li>
-					<li><a href="#">Tag 2</a></li>
-				</ul>
-			</div>
-        </div>
-   </div>
+		<div class="tag card">
+			<h2 class="title">Tag</h2>
+			<ul>
+				<li><a href="#">Tag 1</a></li>
+				<li><a href="#">Tag 2</a></li>
+			</ul>
+		</div>
+    </div>
 </div>
 @endsection
