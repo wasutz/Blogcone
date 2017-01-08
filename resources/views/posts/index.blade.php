7e065944a8dae358519da2e6c0730e8e9ec7205f
Wasut <wasut_pao@hotmail.com>
2017-01-08 18:02:15 +0700

    change mock my post to loop post of auth user

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-	Hello my post
+	<div class="col-md-8">
+		@foreach ($posts as $post)
+			<div class="card clearfix">
+				<h2 class="title"><a href="">{{ $post->title }}</a></h2>
+
+				<div class="meta">
+					<span>by <a href="#">{{ $post->user->username }}</a></span> -
+					<span>{{ $post->created_at->format('d M Y') }}</span>
+				</div>
+				<p class="content">{{ $post->content }}</p>
+
+				<div class="pull-left">
+					<span>Likes 147</span>
+					<span>Comments 3</span>
+				</div>
+				<button class="btn btn-primary pull-right">Readmore</button>
+			</div>
+		@endforeach
+	</div>
 @endsection
