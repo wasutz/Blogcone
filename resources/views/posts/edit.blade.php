11bebb8f04857845c12af137ca0eb2bcd3a42b21
Wasut <wasut_pao@hotmail.com>
2017-01-08 20:21:42 +0700

    add method put, url posts.update, old value of post to input

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,16 +3,17 @@
 @section('content')
 <div class="form-center">
 	<h2>Edit Post</h2>
-	<form action="" method="post">
+	<form action="{{ route('posts.update', ['id' => $post->id]) }}" method="post">
 		{{ csrf_field() }}
+		<input name="_method" type="hidden" value="PUT">
 
 		<div class="form-group">
 		    <label for="title">Title:</label>
-		    <input type="text" class="form-control" id="title" name="title">
+		    <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}">
 		</div>
 		<div class="form-group">
 		    <label for="content">Content</label>
-		    <textarea name="content" class="form-control" rows="10"></textarea>
+		    <textarea name="content" class="form-control" rows="10" value="{{ $post->content }}"></textarea>
 		</div>
 		<button type="submit" class="btn btn-primary">Save</button>
 	</form>
