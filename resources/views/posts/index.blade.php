ef8e17cf3c2885257e9ad60ff1c0dd09e9df1eee
Wasut <wasut_pao@hotmail.com>
2017-01-10 19:55:56 +0700

    add manage button for edit, delete

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,8 +4,12 @@
 	<div class="col-md-8">
 		@foreach ($posts as $post)
 			<div class="card clearfix">
+				<div class="manage-button pull-right">
+					<a href="{{ route('posts.edit', ['post' => $post])}}"><button class="btn btn-warning">Edit</button></a>
+					<a href=""><button class="btn btn-danger">Delete</button></a>
+				</div>
 				<h2 class="title"><a href="">{{ $post->title }}</a></h2>
-
+						
 				<div class="meta">
 					<span>by <a href="#">{{ $post->user->username }}</a></span> -
 					<span>{{ $post->created_at->format('d M Y') }}</span>
@@ -16,7 +20,6 @@
 					<span>Likes 147</span>
 					<span>Comments 3</span>
 				</div>
-				<button class="btn btn-primary pull-right">Readmore</button>
 			</div>
 		@endforeach
 	</div>
