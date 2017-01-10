118a91fa78b8ca92976a18c72ee240fcfdf6be02
Wasut <wasut_pao@hotmail.com>
2017-01-10 20:48:27 +0700

    add order desc to account posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = Auth::user()->posts()->paginate(7);
+        $posts = Auth::user()->posts()->orderBy('id', 'desc')->paginate(7);
 
         return view('posts.index')->with('posts', $posts);
     }
