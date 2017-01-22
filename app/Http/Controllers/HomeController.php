9322608f0145b4e0a572da6f6506b24e943f9327
Wasut <wasut_pao@hotmail.com>
2017-01-22 20:25:04 +0700

    change paginate to use take instead

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,25 +8,10 @@ use App\Tag;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $posts = Post::orderBy('id', 'desc')->paginate(7);
-        $tags = Tag::paginate(10);
+        $tags = Tag::all()->take(10);
 
         return view('index')->with(['posts' => $posts,
                                     'tags' => $tags]);
