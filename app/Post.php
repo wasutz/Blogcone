499e1fe22c593f38f26e2d20301aec03fdd0c01e
Wasut <wasut_pao@hotmail.com>
2017-01-22 17:35:15 +0700

    add method addTags, refactor code use firstOrCreate instead

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Tag;
 
 class Post extends Model
 {
@@ -12,11 +13,22 @@ class Post extends Model
         'user_id',
     ];
 
-    public function user(){
+    public function user()
+    {
     	return $this->belongsTo('App\User');
     }
 
-    public function tags(){
+    public function tags()
+    {
     	return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id');
+    }
+
+    public function addTags($tags)
+    {
+        foreach($tags as $tag){
+            $find = Tag::firstOrCreate(['name' => $tag]);
+
+            $this->tags()->attach($find);
+        }
     }
 }
