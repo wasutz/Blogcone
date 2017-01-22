83bffc92e8f9fbe42a9b816509da55de0999b23e
Wasut <wasut_pao@hotmail.com>
2017-01-22 19:48:55 +0700

    separate sidebar to patials

---
+++ resources/views/layouts/patials/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="col-md-4">
+	<div class="job card">
+		<h2 class="title">Job</h2>
+		<ul>
+			<li><a href="#">Job 1</a></li>
+			<li><a href="#">Job 2</a></li>
+		</ul>
+	</div>
+	<div class="tag card">
+		<h2 class="title">Tag</h2>
+		<ul>
+			@foreach ($tags as $tag)
+				<li><a href="{{ url("/tags/".$tag->id)}}">{{ $tag->name }}</a></li>
+			@endforeach
+		</ul>
+	</div>
+</div>
