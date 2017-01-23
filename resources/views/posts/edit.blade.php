31e7718780b9c981a4ccf5282369ac78f80b232c
Wasut <wasut_pao@hotmail.com>
2017-01-23 15:26:05 +0700

    add tags input to edit page

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('stylesheets')
+	<link rel="stylesheet" href="/css/select2.min.css">
+@endsection
+
 @section('content')
 <div class="form-center">
 	<h2>Edit Post</h2>
@@ -7,15 +11,54 @@
 		{{ csrf_field() }}
 		<input name="_method" type="hidden" value="PUT">
 
-		<div class="form-group">
+		<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
 		    <label for="title">Title:</label>
 		    <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}">
+
+		    @if ($errors->has('title'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('title') }}</strong>
+                </span>
+            @endif
 		</div>
-		<div class="form-group">
+		<div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
 		    <label for="content">Content</label>
 		    <textarea name="content" class="form-control" rows="10" value="{{ $post->content }}"></textarea>
+
+		    @if ($errors->has('content'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('content') }}</strong>
+                </span>
+            @endif
 		</div>
+		
+		<div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
+		 	<label for="tags">Tags</label>
+			<select id="tags" name="tags[]" class="form-control js-example-tags" multiple="multiple">
+				@foreach($post->tags as $tag)
+					<option value="{{ $tag->name }}" selected="selected">{{ $tag->name }}</option>
+				@endforeach
+			</select>
+
+			@if ($errors->has('tags'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('tags') }}</strong>
+                </span>
+            @endif
+		</div>
+
 		<button type="submit" class="btn btn-primary">Save</button>
 	</form>
 </div>
+@endsection
+
+@section('scripts')
+	<script src="/js/select2.min.js"></script>
+	<script type="text/javascript">
+		$(".js-example-tags").select2({
+		  tags: true,
+		  tokenSeparators: [",", " "],
+		  multiple: true
+		});
+	</script>
 @endsection
