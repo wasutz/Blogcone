890af9475ea818c3a0eae4232a4d17958c58e698
Wasut <wasut_pao@hotmail.com>
2017-02-03 15:28:26 +0700

    add sidebar admin and style

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
+        <div class="col-md-3">
+            @include('layouts.patials.admin-sidebar');
+        </div>
+        <div class="col-md-9">
+            Hellod
         </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/layouts/patials/admin-sidebar.blade.php
@@ -0,0 +1,17 @@
+<ul class="sidebar-nav">
+    <li class="active">
+        <a href="#">Dashboard</a>
+    </li>
+    <li>
+        <a href="#">Reviews</a>
+    </li>
+    <li>
+        <a href="#">Posts</a>
+    </li>
+    <li>
+        <a href="#">Jobs</a>
+    </li>
+    <li>
+        <a href="#">Tags</a>
+    </li>
+</ul>
