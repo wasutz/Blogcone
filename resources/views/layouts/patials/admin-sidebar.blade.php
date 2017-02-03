9fbc9b97ce1dd9a3f66a3aa83d5c801d09834c7b
Wasut <wasut_pao@hotmail.com>
2017-02-03 15:58:01 +0700

    add active by request path

--- resources/views/layouts/patials/admin-sidebar.blade.php
+++ resources/views/layouts/patials/admin-sidebar.blade.php
@@ -1,17 +1,17 @@
 <ul class="sidebar-nav">
-    <li class="active">
-        <a href="#">Dashboard</a>
+    <li class="{{ Request::is("admin") ? "active" : "" }}" >
+        <a href="{{ url('/admin') }}">Dashboard</a>
     </li>
-    <li>
-        <a href="#">Reviews</a>
+    <li class="{{ Request::is("admin/reviews") ? "active" : "" }}" >
+        <a href="{{ url('/admin/reviews') }}">Reviews</a>
     </li>
-    <li>
+    <li class="{{ Request::is("admin/posts") ? "active" : "" }}" >
         <a href="#">Posts</a>
     </li>
-    <li>
+    <li class="{{ Request::is("admin/jobs") ? "active" : "" }}" >
         <a href="#">Jobs</a>
     </li>
-    <li>
+    <li class="{{ Request::is("admin/tags") ? "active" : "" }}" >
         <a href="#">Tags</a>
     </li>
 </ul>
