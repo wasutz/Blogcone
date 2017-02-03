9b1d7d70c14e7e8efa27f9189d428c6e53e0b754
Wasut <wasut_pao@hotmail.com>
2017-02-03 14:50:11 +0700

    change to show only published post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,10 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('id', 'desc')->paginate(7);
+        $posts = Post::where('published', config('post.published'))
+        			 ->orderBy('id', 'desc')
+       				 ->paginate(7);
+       				 
         $tags = Tag::all()->take(10);
 
         return view('index')->with(['posts' => $posts,
