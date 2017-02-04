0a8ff63be51c94e3e2a324cd27c084eab71b9141
Wasut <wasut_pao@hotmail.com>
2017-02-04 21:03:08 +0700

    change word to plural

--- resources/views/layouts/patials/sidebar.blade.php
+++ resources/views/layouts/patials/sidebar.blade.php
@@ -1,13 +1,13 @@
 <div class="col-md-4">
 	<div class="job card">
-		<h2 class="title">Job</h2>
+		<h2 class="title">Jobs</h2>
 		<ul>
 			<li><a href="#">Job 1</a></li>
 			<li><a href="#">Job 2</a></li>
 		</ul>
 	</div>
 	<div class="tag card">
-		<h2 class="title">Tag</h2>
+		<h2 class="title">Tags</h2>
 		<ul>
 			@foreach ($tags as $tag)
 				<li><a href="{{ url("/tags/".$tag->id)}}">{{ $tag->name }}</a></li>
