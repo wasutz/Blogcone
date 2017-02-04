1d856f71810cc7df9f844cebf1022684a7a57087
Wasut <wasut_pao@hotmail.com>
2017-02-04 18:48:05 +0700

    add mock UI review page

--- resources/views/admin/review.blade.php
+++ resources/views/admin/review.blade.php
@@ -5,10 +5,29 @@
         <div class="col-md-3">
             @include('layouts.patials.admin-sidebar');
         </div>
-        <div class="col-md-9">
+        <div class="card col-md-9">
             <h1>Review Posts</h1>
 
-            
+            <table class="table table-striped">
+              <tr>
+                <th>Title</th>
+                <th>Action</th>
+              </tr>
+              @foreach($posts as $post)
+                <tr>
+                  <td class="col-sm-8">{{ $post->title }}</td>
+                  <td class="col-sm-4">
+                    <button class="btn btn-success btn-sm">Publish</button>
+                    <button class="btn btn-warning btn-sm">Cancel</button>
+                    <button class="btn btn-danger btn-sm">Delete</button>
+                  </td>
+                </tr>
+              @endforeach
+            </table>
+
+            <div class="col-md-12 text-center">
+              {!! $posts->links() !!}
+            </div>
         </div>
     </div>
 @endsection
