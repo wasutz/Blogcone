c5ffc0123816c9870c8d41d656f3dfeef2c116e1
Wasut <wasut_pao@hotmail.com>
2017-02-04 19:49:00 +0700

    add href to dashboard block

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,27 +8,31 @@
         <div class="card col-md-9">
             <h1>Dashboard</h1>
 
-            <div class="col-md-4">
-                <div class="panel panel-danger">
-                 <div class="panel-heading">
-                    <h3 class="panel-title">Posts in Review</h3>
+            <a href="{{ route('admin.reviews') }}">
+              <div class="col-md-4">
+                  <div class="panel panel-danger">
+                   <div class="panel-heading">
+                      <h3 class="panel-title">Posts in Review</h3>
+                    </div>
+                    <div class="panel-body">
+                      <h1 class="pull-right">{{$postInReview}}</h1>
+                    </div>
                   </div>
-                  <div class="panel-body">
-                    <h1 class="pull-right">{{$postInReview}}</h1>
-                  </div>
-                </div>
-            </div>
+              </div>
+            </a>
 
-            <div class="col-md-4">
-                <div class="panel panel-success">
-                 <div class="panel-heading">
-                    <h3 class="panel-title">Published Posts</h3>
+            <a href="{{ route('admin.posts') }}">
+              <div class="col-md-4">
+                  <div class="panel panel-success">
+                   <div class="panel-heading">
+                      <h3 class="panel-title">Published Posts</h3>
+                    </div>
+                    <div class="panel-body">
+                      <h1 class="pull-right">{{$postPublished}}</h1>
+                    </div>
                   </div>
-                  <div class="panel-body">
-                    <h1 class="pull-right">{{$postPublished}}</h1>
-                  </div>
-                </div>
-            </div>
+              </div>
+            </a>
 
             <div class="col-md-4">
                 <div class="panel panel-primary">
