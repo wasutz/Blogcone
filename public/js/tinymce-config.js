174ae9dafea842d0b47f5b66ab475b3eb5cb8ff4
Wasut <wasut_pao@hotmail.com>
2017-02-11 18:16:15 +0700

    add default folder for upload image

---
+++ public/js/tinymce-config.js
@@ -0,0 +1,26 @@
+tinymce.init({ 
+	selector: 'textarea',
+	plugins: 'link image preview code hr',
+	menubar: false,
+	relative_urls: false,
+	file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = "/" + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+});
