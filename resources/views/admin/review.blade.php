68bc8b638164f98f1abb8e23550fbe928878af6b
Wasut <wasut_pao@hotmail.com>
2017-02-11 17:39:39 +0700

    add no posts message

--- resources/views/admin/review.blade.php
+++ resources/views/admin/review.blade.php
@@ -8,27 +8,31 @@
         <div class="card col-md-9">
             <h1>Review Posts</h1>
 
-            <table class="table table-striped">
-              <tr>
-                <th>Title</th>
-                <th>Action</th>
-              </tr>
-              @foreach($posts as $post)
+            @if($posts->count() > 0)
+              <table class="table table-striped">
                 <tr>
-                  <td class="col-sm-8">{{ $post->title }}</td>
-                  <td class="col-sm-4">
-                    <form class="inline-block" action="{{ route('posts.review', ["id"=> $post->id]) }}" method="post">
-                      {{ csrf_field() }}
-                      <button class="btn btn-success btn-sm">Publish</button>
-                    </form>
-                     <form class="inline-block" action="{{ route('posts.cancel', ["id"=> $post->id]) }}" method="post">
-                         {{ csrf_field() }}
-                         <button class="btn btn-warning btn-sm">Cancel</button>
-                     </form>
-                  </td>
+                  <th>Title</th>
+                  <th>Action</th>
                 </tr>
-              @endforeach
-            </table>
+                @foreach($posts as $post)
+                  <tr>
+                    <td class="col-sm-8">{{ $post->title }}</td>
+                    <td class="col-sm-4">
+                      <form class="inline-block" action="{{ route('posts.review', ["id"=> $post->id]) }}" method="post">
+                        {{ csrf_field() }}
+                        <button class="btn btn-success btn-sm">Publish</button>
+                      </form>
+                       <form class="inline-block" action="{{ route('posts.cancel', ["id"=> $post->id]) }}" method="post">
+                           {{ csrf_field() }}
+                           <button class="btn btn-warning btn-sm">Cancel</button>
+                       </form>
+                    </td>
+                  </tr>
+                @endforeach
+              </table>
+            @else
+              <h4 class="text-center margin-up-3">No Posts in Review</h4>
+            @endif
 
             <div class="col-md-12 text-center">
               {!! $posts->links() !!}
