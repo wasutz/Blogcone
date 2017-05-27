81921cce69ff4820f21b1e58c4dfa508ccf809b6
Wasut <wasut_pao@hotmail.com>
2017-05-27 15:13:37 +0700

    delete id, timestamp from Like, add primary key to all remaining

--- database/migrations/2017_01_24_134004_create_likes_table.php
+++ database/migrations/2017_01_24_134004_create_likes_table.php
@@ -14,11 +14,10 @@ class CreateLikesTable extends Migration
     public function up()
     {
         Schema::create('likes', function (Blueprint $table) {
-            $table->increments('id');
             $table->integer('user_id');
             $table->integer('likeable_id');
             $table->string('likeable_type');
-            $table->timestamps();
+            $table->primary(['user_id', 'likeable_id', 'likeable_type']);
         });
     }
 
