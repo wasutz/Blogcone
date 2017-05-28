49ae9ae0ae41854eea3536c71cae818bf317910a
Wasut <wasut_pao@hotmail.com>
2017-05-28 21:34:01 +0700

    add method archives, scopeFilters to get archives

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Tag;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -82,6 +83,27 @@ class Post extends Model
     {
         $this->published = $published;
         $this->save();
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        if($month = $filters['month']){
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if($year = $filters['year']){
+            $query->whereYear('created_at', $year);
+        }
+    }
+
+    public static function archives()
+    {
+        return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+                     ->where('published', config('post.published'))
+                     ->groupBy('year', 'month')
+                     ->orderByRaw('min(created_at) desc')
+                     ->get()
+                     ->toArray();
     }
 
     public static function getPostByStatus($status)
