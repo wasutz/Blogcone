f077355d64c134f443985eafd46d8628d781d7a6
Wasut <wasut_pao@hotmail.com>
2017-05-28 20:02:21 +0700

    refactor spacing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,19 +5,32 @@
 	<div class="col-md-8">
 		@foreach ($posts as $post)
 			<div class="post card clearfix">
-				<h2 class="title"><a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post->title }}</a></h2>
+				<h2 class="title">
+					<a href="{{ route('posts.show', ['post' => $post]) }}">
+						{{ $post->title }}
+					</a>
+				</h2>
+
 				<div class="meta">
 					<span>by <a href="#">{{ $post->user->username }}</a></span> -
 					<span>{{ $post->created_at->format('d M Y') }}</span> -
 					<span>Tags: </span>
+
 					@foreach($post->tags as $tag)
-						<a href="{{ url('/tags/'. $tag->id)}}"><span>{{ $tag->name }}</span></a>
+						<a href="{{ url('/tags/'. $tag->id)}}">
+							<span>{{ $tag->name }}</span>
+						</a>
 					@endforeach
 				</div>
-				<p class="content">{{ substr(strip_tags($post->content), 0, 300) }}</p>
+
+				<p class="content">
+					{{ substr(strip_tags($post->content), 0, 300) }}
+				</p>
 
 				<div class="pull-left">
-					<form class="inline-block" action="{{ route('posts.like', ["id" => $post->id]) }}" method="post">
+					<form class="inline-block" 
+						  action="{{ route('posts.like', ["id" => $post->id]) }}" method="post">
+
 						@if (Auth::guest())
 							<span class="like-button glyphicon glyphicon-heart-empty"></span>
 						@else
@@ -26,7 +39,9 @@
 						@endif
 						
 						<span class="likes-count">{{ $post->getLikes() }}</span>
+
 						{{ csrf_field() }}
+
 					</form>
 
 					<div class="comments-count inline-block">
