1eaa51ae9b4794dca7bcd21208bc5220c8fe6a1d
Wasut Panyawiphat <wasut_pao@hotmail.com>
2019-10-13 14:24:43 +0700

    remove duplicate code by moving code to partial view

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -3,22 +3,9 @@
 @section('content')
 <div class="row">
 	<div class="col-md-8">
-		<h2>{{ $tag->name }}</h2>
+		<h2>Tag: {{ $tag->name }}</h2>
 		@foreach ($posts as $post)
-			<div class="card clearfix">
-				<h2 class="title"><a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post->title }}</a></h2>
-				<div class="meta">
-					<span>by <a href="#">{{ $post->user->username }}</a></span> -
-					<span>{{ $post->created_at->format('d M Y') }}</span>
-				</div>
-				<p class="content">{{ $post->content }}</p>
-
-				<div class="pull-left">
-					<span>Likes 147</span>
-					<span>Comments 3</span>
-				</div>
-				<button class="btn btn-primary pull-right">Readmore</button>
-			</div>
+			@include('layouts.patials.post')
 		@endforeach
 	</div>
 		
@@ -26,4 +13,8 @@
 		{!! $posts->links() !!}
 	</div>
 </div>
+@endsection
+
+@section("scripts")
+	<script src="/js/like.js"></script>
 @endsection
